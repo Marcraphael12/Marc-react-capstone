1dbeaf5dd0634215d6e0da8abe53d3b49ad7e538
Marc raphael <marcraphael979@gmail.com>
2022-02-12 00:40:48 +0100

    index: Import the App and reducer

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import store from './Redux/configureStore';
+import { loadDataThunk } from './Redux/covid/covid';
+import './scss/index.scss';
+import './css/index.css';
+
+store.dispatch(loadDataThunk());
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
