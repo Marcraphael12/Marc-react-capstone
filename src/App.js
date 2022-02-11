3995653aafbd8c594e3806755b0217f141285664
Marc raphael <marcraphael979@gmail.com>
2022-02-12 00:40:26 +0100

    App: Import every component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Nav';
+import Countries from './components/countries/Countries';
+import CountryInfo from './components/countries/country_infos';
+import { dataLoading, loadDataThunk, selectData } from './Redux/covid/covid';
+import countryNames from './components/countries/countries_name';
+import Footer from './components/Footer';
+import './scss/app.scss';
 
 function App() {
+  const countries = useSelector((state) => state.covidReducer.countries);
+  const total = useSelector((state) => state.covidReducer.total);
+  const load = useSelector((state) => state.covidReducer.loading);
+  const currentCntry = useSelector((state) => state.covidReducer.currentCountry);
+  const isoName = countryNames[currentCntry];
+
+  const mapImg = isoName ? `https://raw.githubusercontent.com/djaiss/mapsicon/33ba28808f8d32b5bae0ffada9cadd07073852e1/all/${isoName.toLowerCase()}/vector.svg` : '/World_map.png';
+  const dispatch = useDispatch();
+
+  const handleClick = (country) => dispatch(selectData(country));
+  useEffect(() => {
+    dispatch(dataLoading());
+    dispatch(loadDataThunk());
+  }, [dispatch]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Countries countries={countries} total={total} loading={load} handleClick={handleClick} />} />
+        <Route path="/country" element={<CountryInfo current={currentCntry} image={mapImg} />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default App;;
