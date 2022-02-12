86669f7ac2bf682348e33a534d185a28aa078d9c
Marc raphael <marcraphael979@gmail.com>
2022-02-12 15:33:34 +0100

    components/Nav: Add back button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { FaVirus } from 'react-icons/fa';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { BsChevronLeft } from 'react-icons/bs';
 import './scss/nav.scss';
 import './countries/scss/countries.scss';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const HandleRoute = (route) => {
+    navigate(route);
+  };
+  const location = useLocation();
   return (
     <nav>
       <div className="location-path">
+      {(location.pathname !== '/') ? <button className="back" type="button" onClick={() => {
+          HandleRoute('/');
+        }}><BsChevronLeft className="fas fa-chevron-left"> </BsChevronLeft></button> : <span className="back"></span>}
+        {' '}
         <FaVirus className="icon-virus" />
         <h1 className="back">Covid-19 world statistics</h1>
       </div>
